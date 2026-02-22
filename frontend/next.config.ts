9b0f9eeb2b82d963b096b0a22b4e338324f4945d
seme0011 <remisemenzin@gmail.com>
2026-02-22 19:51:18 +0100

    feat: redirect from onepiece-index.com to poneglyph.fr

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -17,5 +17,31 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'onepiece-index.com',
+          },
+        ],
+        destination: 'https://poneglyph.fr/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.onepiece-index.com',
+          },
+        ],
+        destination: 'https://poneglyph.fr/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 export default nextConfig;
