7bc2a5f3e670e0e242b5c0f3a6da5176b4d33c92
Remidesbois <remisemenzin@gmail.com>
2026-03-25 15:28:09 +0100

    chore: updated schema dump

--- backend/sql/add_gemini_embedding.sql
+++
@@ -1,5 +0,0 @@
-create extension if not exists vector;
-
-alter table pages add column if not exists embedding_gemini halfvec(3072);
-
-create index if not exists pages_embedding_gemini_idx on pages using hnsw (embedding_gemini halfvec_cosine_ops);
--- backend/sql/enable_mangas.sql
+++
@@ -1,1 +0,0 @@
-alter table mangas add column if not exists enabled boolean not null default true;
